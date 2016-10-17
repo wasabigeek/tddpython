bd2b8a4f5d344b553f14487b9a0a1a38b55612aa
Nicholas Koh <mynewbeginning@gmail.com>
2016-10-17 14:28:47 +0800

    Add model for lists

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Create your views here.
@@ -12,6 +12,8 @@ def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
 
+
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')

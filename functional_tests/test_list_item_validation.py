cf7b5d4dea55a11183890db188cddd455525eeb6
Nicholas Koh <mynewbeginning@gmail.com>
2017-01-05 14:54:32 +0800

    list view validations, refactor hardcoded urls

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -28,4 +28,4 @@ class ItemValidationTest(FunctionalTest):
         # He corrects it by filling some text in
         self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
         self.check_for_row_in_list_table('1: Buy milk')
-        self.check_for_row_in_list_table('2: Buy tea')
+        self.check_for_row_in_list_table('2: Make tea')

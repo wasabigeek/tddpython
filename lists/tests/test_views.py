4f27d77e173554690f1cced7e0018c32a104e442
Nicholas Koh <mynewbeginning@gmail.com>
2017-01-24 05:34:31 +0800

    Refactor forms

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,7 +6,10 @@ from django.utils.html import escape
 
 from lists.views import home_page
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    ItemForm, ExistingListItemForm,
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR
+)
 
 
 # Create your tests here.
@@ -80,7 +83,7 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
 
     def test_for_invalid_input_shows_error_on_page(self):
@@ -88,10 +91,24 @@ class ListViewTest(TestCase):
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
 
+    def test_duplicate_item_validation_errors_end_up_on_list_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            '/lists/{0}/'.format(list1.id),
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
+
+
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/{0}/'.format(list_.id))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
 

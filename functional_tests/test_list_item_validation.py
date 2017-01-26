e56d82219f91f332087424903654756f47e4c1f2
Nicholas Koh <mynewbeginning@gmail.com>
2017-01-26 12:21:32 +0800

    add jquery, qunit tests, list.js with keypress listeners

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,6 +3,10 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
+
     def test_cannot_add_empty_list_items(self):
         # Wasabi goes to the home page and tries to submit an empty list item
         # He hits enter on the empty input box
@@ -44,6 +48,20 @@ class ItemValidationTest(FunctionalTest):
 
         # He sees a helpful error message
         self.check_for_row_in_list_table('1: Buy wellies')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You've already got this in your list")
 
+    def test_error_messages_are_cleared_on_input(self):
+        # wasabi starts a list and causes a validation error
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Banter too thick\n')
+        self.check_for_row_in_list_table('1: Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick\n')
+
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        # He types in the input box and the error clears
+        self.get_item_input_box().send_keys('a')
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())

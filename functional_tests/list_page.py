8ecca31bd4df15846e049d5dccb3745ad1b3516c
Nicholas Koh <mynewbeginning@gmail.com>
2017-04-19 22:35:08 +0800

    Create Page object for list pages, use in sharing FT

---
+++ functional_tests/list_page.py
@@ -0,0 +1,46 @@
+from selenium.webdriver.common.keys import Keys
+
+from .base import wait
+
+
+class ListPage(object):
+
+    def __init__(self, test):
+        self.test = test
+
+    def get_item_input_box(self):
+        return self.test.browser.find_element_by_id('id_text')
+
+    def get_table_rows(self):
+        return self.test.browser.find_elements_by_css_selector('#id_list_table tr')
+
+    @wait
+    def wait_for_row_in_list_table(self, item_number, item_text):
+        row_text = '{}: {}'.format(item_number, item_text)
+        rows = self.get_table_rows()
+        self.test.assertIn(row_text, [row.text for row in rows])
+
+    def add_list_item(self, item_text):
+        new_item_no = len(self.get_table_rows()) + 1
+        self.get_item_input_box().send_keys(item_text)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table(new_item_no, item_text)
+        return self
+
+    def get_share_box(self):
+        return self.test.browser.find_element_by_css_selector(
+            'input[name="sharee"]'
+        )
+
+    def get_shared_with_list(self):
+        return self.test.browser.find_elements_by_css_selector(
+            '.list-sharee'
+        )
+
+    def share_list_with(self, email):
+        self.get_share_box().send_keys(email)
+        self.get_share_box().send_keys(Keys.ENTER)
+        self.test.wait_for(lambda: self.test.assertIn(
+            email,
+            [item.text for item in self.get_shared_with_list()]
+        ))
